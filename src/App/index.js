d83b810546955eaf6e613b50c8672dfd53a6ad38
Israa91 <eng.israa.1991@gmail.com>
2021-03-25 16:28:57 +0200

    use craco to change antd theme Relates 32

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './style.css';
+import { Button } from 'antd';
+import './style.less';
 
 function App() {
   return (
     <div className="App">
       <h1>Ws Booker</h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
