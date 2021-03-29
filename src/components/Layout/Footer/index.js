c16d970cc6b2086c40637cf211dc1d0fdb943f35
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-03-29 14:57:22 +0300

    create the Footer with social media icons, copyright, and make some styles and Responsive Relates #6

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  FacebookFilled,
+  TwitterCircleFilled,
+  InstagramFilled,
+} from '@ant-design/icons';
+
+import './style.css';
 
 const Footer = () => (
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <footer className="footer">
+    <div className="icons-list">
+      <Link to="https://www.facebook.com/">
+        <FacebookFilled className="fb" />
+      </Link>
+      <Link to="https://www.twitter.com/">
+        <TwitterCircleFilled className="tw" />
+      </Link>
+      <Link to="https://www.instagram.com/">
+        <InstagramFilled className="ins" />
+      </Link>
+    </div>
+    <div className="copyright">&copy; 2021 WSBooker</div>
+  </footer>
 );
-
 export default Footer;
