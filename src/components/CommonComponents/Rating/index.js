aa28c89c705b3feb90c8a65f2df7c04677ab9819
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-03-29 13:20:19 +0300

    create state setValue, render adding value for rate and set it, handle setting the value for the rate from props Relates #8

--- src/components/CommonComponents/Rating/index.js
+++ src/components/CommonComponents/Rating/index.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 import { Rate } from 'antd';
 
-const Rating = ({ rateValue, setRate }) => {};
+const Rating = ({ rateValue, setRate }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (val) => {
+    setValue(val);
+    setRate(val);
+  };
+
+  return (
+    <span>
+      {rateValue === -1 ? (
+        <Rate onChange={handleChange} value={value} />
+      ) : (
+        <Rate value={rateValue} />
+      )}
+    </span>
+  );
+};
 export default Rating;
