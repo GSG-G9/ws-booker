7a3d48b983652730631599941d146480d58a8971
Sondos-Alalami <sondos.alalami@gmail.com>
2021-03-30 03:05:14 +0300

    handled different sizes of workspace card component Relates #10

--- src/components/CommonComponents/WorkspaceCard/index.js
+++ src/components/CommonComponents/WorkspaceCard/index.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import './style.css';
 import { Card } from 'antd';
 import PropTypes from 'prop-types';
+import { useMediaQuery } from 'react-responsive';
 import Button from '../Button';
 import Rating from '../Rating';
 
 function WorkspaceCard({
+  size,
   id,
   name,
   image,
@@ -17,12 +19,13 @@ function WorkspaceCard({
   reviewers,
   onClick,
 }) {
+  const matches = useMediaQuery({ query: '(max-width:600px)' });
   return (
     <Card
       hoverable
       style={{
-        width: 985,
-        height: 350,
+        width: size === 'small' || matches ? 350 : 985,
+        height: size === 'small' || matches ? 250 : 350,
         display: 'flex',
         flexDirection: 'row',
       }}
@@ -33,23 +36,50 @@ function WorkspaceCard({
             style={{
               borderRadius: '5px',
             }}
-            className="cover-image"
+            className={
+              size === 'small' || matches ? 'small-cover-image' : 'cover-image'
+            }
             src={image}
           />
         </Link>
       }
     >
-      <div className="info-container">
-        <p className="price">
+      <div
+        className={
+          size === 'small' || matches
+            ? 'small-info-container'
+            : 'info-container'
+        }
+      >
+        <p className={size === 'small' || matches ? 'small-price' : 'price'}>
           ₪ {feesPerDay} - Day - ₪ {feesPerHour} - Hour
         </p>
-        <p className="workspace-name">{name}Gaza Sky Geeks</p>
-        <p className="location"> {location}</p>
+        <p
+          className={
+            size === 'small' || matches
+              ? 'small-workspace-name'
+              : 'workspace-name'
+          }
+        >
+          {name}
+        </p>
+        <p
+          className={
+            size === 'small' || matches ? 'small-location' : 'location'
+          }
+        >
+          {' '}
+          {location}
+        </p>
         <Rating rateValue={rating} />
-        <p className="reviewers">{reviewers} Reviewers</p>
+        {size === 'small' || matches ? (
+          <br />
+        ) : (
+          <p className="reviewers">{reviewers} Reviewers</p>
+        )}
         <Button
           buttName="Book Now"
-          width="150px"
+          width={size === 'small' || matches ? '100px' : '150px'}
           fontWeight="bold"
           onClick={onClick}
         />
@@ -59,6 +89,7 @@ function WorkspaceCard({
 }
 
 WorkspaceCard.propTypes = {
+  size: PropTypes.string,
   id: PropTypes.number,
   name: PropTypes.string,
   image: PropTypes.string,
@@ -71,6 +102,7 @@ WorkspaceCard.propTypes = {
 };
 
 WorkspaceCard.defaultProps = {
+  size: '',
   id: '',
   name: '',
   image: '',
