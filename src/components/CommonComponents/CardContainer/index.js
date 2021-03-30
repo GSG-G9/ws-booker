d9d3fd472e6a23f53b2c3a30e23e3c0bd09623ff
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-03-30 23:50:57 +0300

    create CardContainer and render the title and cards Relates #11

---
+++ src/components/CommonComponents/CardContainer/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Typography, Empty } from 'antd';
+import PropTypes from 'prop-types';
+import WorkspaceCard from '../WorkspaceCard';
+import './style.css';
+
+const { Title, Text } = Typography;
+
+const CardContainer = ({ title, searchText, data, size }) => (
+  <div>
+    {searchText ? (
+      <>
+        <Title>{title}</Title>
+        <Text>{searchText}</Text>
+      </>
+    ) : (
+      <Title>{title}</Title>
+    )}
+
+    <div className="cardcontainer">
+      <ul>
+        {data.length !== 0 ? (
+          data.map((item) => (
+            <li key={item.id}>
+              <WorkspaceCard {...item} size={size} />
+            </li>
+          ))
+        ) : (
+          <Empty description="Sorry! There is no Workspaces." />
+        )}
+      </ul>
+    </div>
+  </div>
+);
+CardContainer.defaultProps = {
+  title: '',
+  searchText: '',
+  size: '',
+  data: [],
+};
+
+CardContainer.propTypes = {
+  title: PropTypes.string,
+  searchText: PropTypes.string,
+  size: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      image: PropTypes.string,
+      feesPerDay: PropTypes.number,
+      feesPerHour: PropTypes.number,
+      location: PropTypes.string,
+      rating: PropTypes.number,
+      reviewers: PropTypes.number,
+      onClick: PropTypes.func,
+    })
+  ),
+};
+
+export default CardContainer;
