4850e3501a12a9ed0ba578c2502c5f1655f8bca5
Sondos-Alalami <sondos.alalami@gmail.com>
2021-04-01 11:01:28 +0300

    handled error cases with try catch

--- src/firebase/context.js
+++ src/firebase/context.js
@@ -5,12 +5,19 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser);
+    try {
+      firebase.auth().onAuthStateChanged(setUser);
+    } catch (err) {
+      setError(err);
+    }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, error }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
