caaf57acdc94eeb1c0ff2ef3e4eee803683958c4
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-04-05 12:22:42 +0300

    solve conflict Relates #61

--- src/firebase/firestore/user/index.js
+++ src/firebase/firestore/user/index.js
@@ -1,10 +1,5 @@
 import getUserById from './getUserById';
-<<<<<<< HEAD
 import EditUserData from './EditUserData';
-
-export { getUserById, EditUserData };
-=======
 import addUser from './addUser';
 
-export { getUserById, addUser };
->>>>>>> 6465f62640a7de406a0591a25253ae78a05bfe07
+export { getUserById, EditUserData, addUser };
