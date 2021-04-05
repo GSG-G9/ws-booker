0c8ab9c74682959233d2597fda2961568fc37f83
Israa91 <eng.israa.1991@gmail.com>
2021-04-05 11:51:21 +0300

    remove consol log statements Relates #75

--- src/firebase/firestore/user/addUser.js
+++ src/firebase/firestore/user/addUser.js
@@ -4,20 +4,16 @@ const addUser = async (user) => {
   try {
     const response = db.collection('users').doc(user.uid);
     const doc = await response.get();
-    if (doc.exists) {
-      console.log('already here');
-    } else {
+    if (!doc.exists) {
       doc.set({
         name: user.displayName,
         image: user.photoURL,
         email: user.email,
         phone_number: user.phoneNumber,
       });
-      console.log('Document successfully written!');
     }
     return doc.data();
   } catch (err) {
-    console.error('Error writing document: ', err);
     return err;
   }
 };
