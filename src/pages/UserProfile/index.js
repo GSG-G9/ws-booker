8584be4de600499d958f973bee9463d676b0ace9
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-04-08 02:26:38 +0300

    import and render  getBookingByUserId and getWorkspaceById function, and set their data in the state, render the data from the state in the elements Relates #21

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -3,8 +3,9 @@ import propTypes from 'prop-types';
 import { Image, Typography } from 'antd';
 
 import { getUserById } from '../../firebase/firestore/user';
-
-import WorkspaceCard from '../../components/CommonComponents/WorkspaceCard';
+import getBookingByUserId from '../../firebase/firestore/booking';
+import { getWorkspaceById } from '../../firebase/firestore/workspace';
+// import WorkspaceCard from '../../components/CommonComponents/WorkspaceCard';
 import CardContainer from '../../components/CommonComponents/CardContainer';
 
 import coverImage from '../../assets/images/backgound_cover.png';
@@ -19,24 +20,28 @@ const { Title, Text } = Typography;
 
 const UserProfile = ({ match }) => {
   const [userData, setUserData] = useState({});
+  const [workspaceData, setWorkspaceData] = useState({});
   const { userId } = match.params;
-  console.log('userrrrr', userId);
 
   useEffect(async () => {
     let isActive = 'true';
     if (isActive) {
-      // const UserData = await getUserById('TiXV6ZR2z4YKB5ovaGmm287iqF83');
-      const UserData = await getUserById();
-      console.log('data', UserData);
+      const UserData = await getUserById(userId);
       setUserData(UserData);
+      const bookingbyUserId = await getBookingByUserId(userId);
+      const workspaceId = bookingbyUserId.workspace_id.id;
+      const wsData = await getWorkspaceById(workspaceId);
+      console.log('wsData', wsData);
+      setWorkspaceData(wsData);
     }
     return () => {
       isActive = 'false';
     };
   }, []);
-  console.log('Stat222222e', userData);
+  console.log('statee', workspaceData);
   return (
     <div>
+      <h1>hi from page</h1>
       <div className="profile-main-header">
         <div className="back-profile-header">
           <Image
@@ -47,7 +52,7 @@ const UserProfile = ({ match }) => {
           />
           <div className="user-data">
             <div className="username-section">
-              <Title className="username">Sara Ahmed</Title>
+              <Title className="username">{userData.name}</Title>
               <Image
                 preview={false}
                 src={editico}
@@ -57,38 +62,42 @@ const UserProfile = ({ match }) => {
             </div>
             <div className="email-section">
               <Image preview={false} src={emailico} alt="email" />
-              <Text className="email">sara@gmail.com</Text>
+              <Text className="email">{userData.email}</Text>
             </div>
             <div className="phone-section">
               <Image preview={false} src={phoneico} alt="phone" />
-              <Text className="phone">0599123456</Text>
+              <Text className="phone">{userData.phone_number}</Text>
             </div>
           </div>
         </div>
         <div className="front-profile-header">
           <Image
             preview={false}
-            src={user}
+            src={userData.image}
             alt="user image"
             className="userimage"
           />
         </div>
       </div>
-      <div className="user-ws-section">
-        {/* <Title>My workspace</Title>
+      {/* <div className="user-ws-section"> */}
+      {/* <Title>My workspace</Title>
       <WorkspaceCard /> */}
-        {/* <CardContainer title="My Workspace" size="large"  data={
-          size= 'large',
-          id='editbtn',
-          name = '',
-          image,
-          feesPerDay,
-          feesPerHour,
-          location,
-          rating,
-          reviewers,
-      }/> */}
-      </div>
+      <CardContainer
+        title="My Workspace"
+        size="large"
+        data={[
+          {
+            name: workspaceData.name,
+            feesPerDay: workspaceData.fees_per_day,
+            feesPerHour: workspaceData.fees_per_hour,
+            rating: workspaceData.rating,
+            reviewers: workspaceData.reviewers,
+            location: workspaceData.location,
+            image: workspaceData.header_image,
+          },
+        ]}
+      />
+      {/* </div> */}
     </div>
   );
 };
