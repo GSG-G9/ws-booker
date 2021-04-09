208373e311449391b8c80a413253c931632ec7cc
Israa91 <eng.israa.1991@gmail.com>
2021-04-09 18:07:08 +0300

    make style responsive Relates #17

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { SearchOutlined } from '@ant-design/icons';
-
+import { Typography } from 'antd';
 import homeheader from '../../assets/images/homeheader.jpg';
 import MainButton from '../CommonComponents/Button';
 import MainInput from '../CommonComponents/Input';
 import { createSearchUrl } from '../../utils';
 import './style.css';
 
+const { Title } = Typography;
 const HomeHeader = () => {
   const history = useHistory();
   const [city, setCity] = useState('');
@@ -38,41 +39,39 @@ const HomeHeader = () => {
   return (
     <div>
       <div className="header_image ">
-        <img src={homeheader} alt="ar" className="fill" />
-      </div>
-      <div className="outer_div">
-        <p className="header_title"> The Future of Work has Arrived </p>
+        {/* <img src={homeheader} alt="ar" className="fill" /> */}
+
+        <Title className="header_title"> The Future of Work has Arrived </Title>
         <div className="search_div">
           <MainInput
-            className="search_items"
+            className="search_items search_input"
             type="search"
             placeholder="Search by workspace name..."
             size="large"
             onChange={handleCityChange}
-            style={{ width: '350px ', height: '85px', fontSize: '26px' }}
+            // style={{ width: '350px ', height: '85px', fontSize: '26px' }}
           />
           <MainInput
-            className="search_items"
+            className="search_items date_input"
             type="date"
             placeholder="Start Date"
             size="large"
             onChange={handleDateChange}
-            style={{ width: '215px ', height: '85px', fontSize: '26px' }}
+            // style={{ width: '215px ', height: '85px', fontSize: '26px' }}
           />
           <MainInput
-            className="search_items"
+            className="search_items number_input"
             type="number"
             placeholder="Num of people"
             size="large"
             onChange={handleNumberChange}
-            style={{ width: '215px ', height: '85px' }}
+            // style={{ width: '215px ', height: '85px' }}
           />
           <MainButton
             buttName="Search"
             className="search_button"
             size="large"
             onClick={handleButtonClick}
-            style={{ width: '200px ', height: '85px', fontSize: '26px' }}
             icon={<SearchOutlined />}
           />
         </div>
