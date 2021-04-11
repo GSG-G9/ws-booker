c3945c56001db9a0a88f37aec3f353673632ac7c
Israa91 <eng.israa.1991@gmail.com>
2021-04-11 22:31:16 +0300

    make search page Relates #19

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { getSearchResults } from '../../firebase/firestore/workspace';
 
+getSearchResults(null, 10).then((res) => console.log(res));
 const About = () => (
   <div>
     <h1>Hi from About page</h1>
