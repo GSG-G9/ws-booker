2a666f00b2114fc51f9a33bb149fe5f022d46645
Israa91 <eng.israa.1991@gmail.com>
2021-04-12 19:41:56 +0300

    edit get search results function

--- src/firebase/firestore/workspace/getSearchResults.js
+++ src/firebase/firestore/workspace/getSearchResults.js
@@ -1,12 +1,58 @@
 import { db } from '../../config';
 
-const getSearchResults = async (city, capacity) => {
+const getSearchResults = async (name, city, capacity) => {
   try {
-    if (city != null && capacity != null) {
+    if (name && city && capacity) {
       const data = await db
         .collection('workspaces')
+        .where('name', '==', name)
         .where('city', '==', city)
         .where('capacity', '>=', Number(capacity))
+        .get();
+      if (!data) {
+        return new Error('No data returned!');
+      }
+      const result = data.docs.map((doc) => doc.data());
+      return result;
+    }
+    if (name && city) {
+      const data = await db
+        .collection('workspaces')
+        .where('name', '==', name)
+        .where('city', '==', city)
+        .get();
+      if (!data) {
+        return new Error('No data returned!');
+      }
+    }
+    if (name && capacity) {
+      const data = await db
+        .collection('workspaces')
+        .where('name', '==', name)
+        .where('capacity', '==', capacity)
+        .get();
+      if (!data) {
+        return new Error('No data returned!');
+      }
+      const result = data.docs.map((doc) => doc.data());
+      return result;
+    }
+    if (city && capacity) {
+      const data = await db
+        .collection('workspaces')
+        .where('capacity', '==', capacity)
+        .where('city', '==', city)
+        .get();
+      if (!data) {
+        return new Error('No data returned!');
+      }
+      const result = data.docs.map((doc) => doc.data());
+      return result;
+    }
+    if (name) {
+      const data = await db
+        .collection('workspaces')
+        .where('name', '==', name)
         .get();
       if (!data) {
         return new Error('No data returned!');
