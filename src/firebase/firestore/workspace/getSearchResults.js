df75cd306e00944ff3a3c7024c6f99c3cc776a3f
Israa91 <eng.israa.1991@gmail.com>
2021-04-19 10:13:12 +0300

    Fix search method by handling case sensitive Relates #100

--- src/firebase/firestore/workspace/getSearchResults.js
+++ src/firebase/firestore/workspace/getSearchResults.js
@@ -8,11 +8,11 @@ const getSearchResults = async (queryObj) => {
       const data = await db
         .collection('workspaces')
         .where('capacity', '>=', Number(capacity))
+        .orderBy('capacity', 'asc')
         .orderBy('name_lower', 'asc')
         .orderBy('city_lower', 'asc')
-        .startAt(q.toLowerCase(), city.toLowerCase())
+        .startAt(capacity, q.toLowerCase(), city.toLowerCase())
         .endAt(`${q.toLowerCase()}\uf8ff`, `${city.toLowerCase()}\uf8ff`)
-
         .get();
       if (!data) {
         return new Error('No data returned!');
@@ -25,10 +25,8 @@ const getSearchResults = async (queryObj) => {
         .collection('workspaces')
         .orderBy('name_lower', 'asc')
         .orderBy('city_lower', 'asc')
-
-        .startAt(city.toLowerCase())
-        .startAt(q.toLowerCase())
-
+        .startAt(q.toLowerCase(), city.toLowerCase())
+        .endAt(`${q.toLowerCase()}\uf8ff`, `${city.toLowerCase()}\uf8ff`)
         .get();
       if (!data) {
         return new Error('No data returned!');
@@ -40,8 +38,9 @@ const getSearchResults = async (queryObj) => {
       const data = await db
         .collection('workspaces')
         .where('capacity', '>=', capacity)
+        .orderBy('capacity', 'asc')
         .orderBy('name_lower', 'asc')
-        .startAt(q.toLowerCase())
+        .startAt(capacity, q.toLowerCase())
         .endAt(`${q.toLowerCase()}\uf8ff`)
         .get();
       if (!data) {
@@ -54,7 +53,10 @@ const getSearchResults = async (queryObj) => {
       const data = await db
         .collection('workspaces')
         .where('capacity', '>=', capacity)
-        .where('city', '==', city)
+        .orderBy('capacity', 'asc')
+        .orderBy('city_lower', 'asc')
+        .startAt(capacity, city.toLowerCase())
+        .endAt(`${city.toLowerCase()}\uf8ff`)
         .get();
       if (!data) {
         return new Error('No data returned!');
@@ -91,7 +93,6 @@ const getSearchResults = async (queryObj) => {
     if (capacity) {
       const data = await db
         .collection('workspaces')
-
         .where('capacity', '>=', Number(capacity))
         .get();
       if (!data) {
@@ -102,7 +103,6 @@ const getSearchResults = async (queryObj) => {
     }
     return null;
   } catch (err) {
-    console.log(err);
     return err;
   }
 };
