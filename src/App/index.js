fa4635466c86fbc5a2fbf99f26d2a97a69f207d0
Sondos-Alalami <sondos.alalami@gmail.com>
2021-04-20 04:23:34 +0300

    handled capacity of workspace at certain time in booking Relates #112

--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,7 @@ import './style.less';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from '../firebase/context';
 import { ProtectedRoutes, AdminRoutes } from './Routes';
+// import { addBooking, postBooking } from '../firebase/firestore/booking';
 import {
   About,
   AllWorkspaces,
@@ -28,6 +29,18 @@ import {
   Layout,
 } from '../pages';
 
+// addBooking(
+//   'joyJK89nRnTPwl7KztnbYFEJ1Yt2',
+//   'tV5kAePWG9nWkxkEHoEv',
+//   1,
+//   'Apr 25 2021 16:05:00',
+//   'Apr 29 2021 17:15:00'
+// );
+// postBooking('joyJK89nRnTPwl7KztnbYFEJ1Yt2', 'tV5kAePWG9nWkxkEHoEv', {
+//   book_capacity: 1,
+//   book_start_time: 'May 2 2021 17:00:00',
+//   book_end_time: 'May 3 2021 18:10:00',
+// }).then(console.log);
 const App = () => (
   <AuthProvider>
     <Router>
--- src/firebase/firestore/booking/postBooking.js
+++ src/firebase/firestore/booking/postBooking.js
@@ -8,15 +8,17 @@ import { getUserById } from '../user';
 import { checkOverlap } from '../../../utils';
 
 const moment = extendMoment(Moment);
-let isOverlapped = false;
-let message;
 
 const postBooking = async (userId, workspaceId, payload) => {
+  let isOverlapped = false;
+  let message;
   try {
     const isWorkspace = await getWorkspaceById(workspaceId);
     if (isWorkspace instanceof Error) {
       throw new Error('This workspace is not exist post');
     }
+    const { capacity } = isWorkspace;
+    console.log('capacityyy', capacity);
     const isUser = await getUserById(userId);
     if (isUser instanceof Error) {
       throw new Error('This user is not exist post');
@@ -32,26 +34,70 @@ const postBooking = async (userId, workspaceId, payload) => {
       bookStartTime,
       bookEndTime
     );
+    console.log(overlappedBookings);
     if (!overlappedBookings.length) {
       isOverlapped = false;
+      message = '';
     } else {
-      const bookingsTimes = overlappedBookings.map(
+      const bookingsDates = overlappedBookings.map(
         ({ book_start_time: startTime, book_end_time: endTime }) => [
-          moment(startTime.toDate()).format('HH:mm:ss'),
-          moment(endTime.toDate()).format('HH:mm:ss'),
+          moment(startTime.toDate()).format('ddd MMM DD YYYY HH:mm:ss'),
+          moment(endTime.toDate()).format('ddd MMM DD YYYY HH:mm:ss'),
         ]
       );
+      console.log('datesss', bookingsDates);
+      const dateTimeArray = [];
+      bookingsDates.forEach((item) => {
+        const start = moment(item[0]).format('HH:mm:ss');
+        const end = moment(item[1]).format('HH:mm:ss');
+        const daysRange = moment.range(item[0], item[1]);
+        const days = Array.from(daysRange.by('day'));
+        const bookingDays = days.map((m) => m.format('ddd MMM DD YYYY'));
+        const dateTimeObject = { time: [start, end], dates: bookingDays };
+        dateTimeArray.push(dateTimeObject);
+      });
+      console.log(dateTimeArray);
+
+      const daysRange = moment.range(bookStartTime, bookEndTime);
+      const days = Array.from(daysRange.by('day'));
+      const newBookingDays = days.map((m) => m.format('ddd MMM DD YYYY'));
+
       const newBookingTimeRange = [
         moment(bookStartTime).format('HH:mm:ss'),
         moment(bookEndTime).format('HH:mm:ss'),
       ];
-
-      bookingsTimes.forEach((range) => {
-        if (checkOverlap([range, newBookingTimeRange])) {
-          message = `Sorry, the time range ${range} is already booked at this date`;
-          isOverlapped = true;
+      const overlappedTimeBooking = [];
+      dateTimeArray.forEach((range) => {
+        if (checkOverlap([range.time, newBookingTimeRange])) {
+          overlappedTimeBooking.push(range);
         }
       });
+      console.log('filterrred', overlappedTimeBooking);
+      if (!overlappedTimeBooking.length) {
+        isOverlapped = false;
+        message = '';
+      } else {
+        const daysCapacity = [];
+        newBookingDays.forEach((newItem) => {
+          let count = 0;
+          overlappedTimeBooking.forEach((item) => {
+            if (item.dates.includes(newItem)) {
+              count += 1;
+            }
+          });
+          daysCapacity.push({ date: newItem, repeat: count });
+        });
+        console.log(daysCapacity);
+        const maxDayRepeat = daysCapacity.reduce((max, obj) =>
+          max.repeat > obj.repeat ? max : obj
+        );
+        console.log(maxDayRepeat);
+        if (maxDayRepeat.repeat >= capacity) {
+          message = `Sorry, the date  ${maxDayRepeat.date} is full at this time`;
+          isOverlapped = true;
+          console.log('trueeeeee');
+        }
+      }
     }
     if (isOverlapped) {
       return {
