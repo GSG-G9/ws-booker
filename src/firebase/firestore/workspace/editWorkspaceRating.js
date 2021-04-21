95e1774ad30c52b8385d9bb893dff604a90e97f8
Israa91 <eng.israa.1991@gmail.com>
2021-04-21 16:18:06 +0300

    remove console

--- src/firebase/firestore/workspace/editWorkspaceRating.js
+++ src/firebase/firestore/workspace/editWorkspaceRating.js
@@ -5,11 +5,9 @@ import { getRatingByWorkspaceId } from '../rating';
 const editWorkspaceRating = async (id) => {
   try {
     const workspaceRate = await getRatingByWorkspaceId(id);
-    console.log('update', workspaceRate);
     await db.collection('workspaces').doc(id).update({
       rating: workspaceRate,
     });
-    console.log('ok');
     return { msg: 'rating updated successfully' };
   } catch (err) {
     return err;
