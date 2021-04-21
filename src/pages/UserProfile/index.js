dd5260bbb5d4073581593a3a9c5c398a3b2e9f72
Sondos-Alalami <sondos.alalami@gmail.com>
2021-04-21 13:31:09 +0300

    handeled info details

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -331,37 +331,46 @@ const UserProfile = ({ match }) => {
       <div className="user-ws-section">
         <div className="my-ws-title-container">
           <Title className="my-ws-title">My workspace</Title>
-          <Title onClick={info} className="show-details-title">
+          {/* <Title onClick={info} className="show-details-title">
             Show details
-          </Title>
+          </Title> */}
         </div>
-        {error ? (
-          <Empty description="Sorry! You don't have any workspace booking!" />
-        ) : isWSLoader ? (
-          <Loader />
-        ) : (
-          <Popconfirm
-            title="Are you sure to delete the booking?"
-            onConfirm={confirm}
-            onCancel={cancel}
-            okText="Yes"
-            cancelText="No"
-            visible={confirmVisible}
-          >
-            <WorkspaceCard
-              id={wsId}
-              name={workspaceData.name}
-              feesPerDay={workspaceData.fees_per_day}
-              feesPerHour={workspaceData.fees_per_hour}
-              rating={workspaceData.rating}
-              reviewers={workspaceData.reviewers}
-              location={workspaceData.location}
-              image={workspaceData.header_image}
-              buttonName="Cancel Book"
-              onClick={() => setConfirmVisible(true)}
-            />
-          </Popconfirm>
-        )}
+        <div className="user-info-card-container">
+          {error ? (
+            <Empty description="Sorry! You don't have any workspace booking!" />
+          ) : isWSLoader ? (
+            <Loader />
+          ) : (
+            <div>
+              <div className="user-details-card-container">
+                <WorkspaceInfo icon={time} text={bookingTime} />
+                <WorkspaceInfo icon={calender} text={bookingDate} />
+                <WorkspaceInfo icon={persons} text={bookingCapacity} />
+              </div>
+              <Popconfirm
+                title="Are you sure to delete the booking?"
+                onConfirm={confirm}
+                onCancel={cancel}
+                okText="Yes"
+                cancelText="No"
+                visible={confirmVisible}
+              >
+                <WorkspaceCard
+                  id={wsId}
+                  name={workspaceData.name}
+                  feesPerDay={workspaceData.fees_per_day}
+                  feesPerHour={workspaceData.fees_per_hour}
+                  rating={workspaceData.rating}
+                  reviewers={workspaceData.reviewers}
+                  location={workspaceData.location}
+                  image={workspaceData.header_image}
+                  buttonName="Cancel Book"
+                  onClick={() => setConfirmVisible(true)}
+                />
+              </Popconfirm>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
